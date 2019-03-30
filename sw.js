90dfc29c48396ec6a05b044aec3af03c55f40e40
Jemima Assim-Ita <daraassimita@gmail.com>
2019-03-30 14:01:54 +0100

    Heroki

--- sw.js
+++ sw.js
@@ -10,7 +10,6 @@ var filesToCache = [
   'https://code.jquery.com/jquery-3.3.1.slim.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-  'login.html'
 ];
 
 // on installation of service worker, add files to the cache
